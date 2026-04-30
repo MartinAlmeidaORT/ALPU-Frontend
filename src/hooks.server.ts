fcaece0f3b4fbb44ebc0de3c35b7f96115077059
Martin Almeida <MA338947@fi365.ort.edu.uy>
2026-04-30 20:43:30 -0300

    feat: cant navigate to login with session cookies

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,9 +4,14 @@ import { createUrqlClient } from '$lib/graphql/client';
 export const handle: Handle = async ({ event, resolve }) => {
   const session = event.cookies.get('session_id');
 
+  const isLoginPage = event.route.id?.includes("login");
   const isProtected = event.route.id?.includes('(protected)');
 
   if (isProtected && !session) {
+    throw redirect(303, '/');
+  }
+
+  if (isLoginPage && session) {
     throw redirect(303, '/');
   }
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,37 @@
 <script lang="ts">
-    import "./layout.css";
-    let { children, data } = $props();
-    import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
-    import { navigationMenuTriggerStyle } from "$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte";
-    import { IsMobile } from "$lib/components/hooks/is-mobile.svelte.js";
+  import './layout.css';
+  let { children, data } = $props();
+  import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
+  import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
+  import { IsMobile } from '$lib/components/hooks/is-mobile.svelte.js';
 
-    let user: any = $state(null);
-    let token: any = $state("");
-    const isMobile = new IsMobile();
-    // let authReady = $state(false);
+  let user: any = $state(null);
+  let token: any = $state('');
+  const isMobile = new IsMobile();
+  // let authReady = $state(false);
 </script>
 
 <div class="w-full flex justify-center py-4">
-    <!-- {#if authReady} -->
-    <NavigationMenu.Root viewport={isMobile.current}>
-        <NavigationMenu.List class="flex-wrap align-items-center">
-            <NavigationMenu.Item>
-                <NavigationMenu.Link>
-                    {#snippet child()}
-                        <a href="/" class={navigationMenuTriggerStyle()}>Home</a
-                        >
-                    {/snippet}
-                </NavigationMenu.Link>
-            </NavigationMenu.Item>
-            <NavigationMenu.Item>
-                <NavigationMenu.Link>
-                    {#snippet child()}
-                        <a href="login" class={navigationMenuTriggerStyle()}
-                            >Sesion</a
-                        >
-                    {/snippet}
-                </NavigationMenu.Link>
-            </NavigationMenu.Item>
-        </NavigationMenu.List>
-    </NavigationMenu.Root>
-    <!-- {/if} -->
+  <!-- {#if authReady} -->
+  <NavigationMenu.Root viewport={isMobile.current}>
+    <NavigationMenu.List class="flex-wrap align-items-center">
+      <NavigationMenu.Item>
+        <NavigationMenu.Link>
+          {#snippet child()}
+            <a href="/" class={navigationMenuTriggerStyle()}>Home</a>
+          {/snippet}
+        </NavigationMenu.Link>
+      </NavigationMenu.Item>
+      <NavigationMenu.Item>
+        <NavigationMenu.Link>
+          {#snippet child()}
+            <a href="/login" class={navigationMenuTriggerStyle()}>Sesion</a>
+          {/snippet}
+        </NavigationMenu.Link>
+      </NavigationMenu.Item>
+    </NavigationMenu.List>
+  </NavigationMenu.Root>
+  <!-- {/if} -->
 </div>
 
 <!-- {#if authReady} -->
